9e46c8abe5c94d5680af4c8115fc1e8671786402
Paul Forness <50929905+ptforness@users.noreply.github.com>
2022-04-24 15:48:42 -0400

    Stop tracking build output

--- lib/routes/spotifyAuth.d.ts
+++
@@ -1,3 +0,0 @@
-import { FastifyInstance } from 'fastify';
-export declare const spotifyAuthRouter: (fastify: FastifyInstance, _: any, done: () => any) => void;
-//# sourceMappingURL=spotifyAuth.d.ts.map
